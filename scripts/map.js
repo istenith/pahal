c34f91b27077c633183906ef6d5bb529ac7e5212
Abhay Raj Singh <abhayonlyone@gmail.com>
2019-07-04 20:27:02 +0530

    minified code, updated designs

--- scripts/map.js
+++ scripts/map.js
@@ -1,14 +1,2 @@
-var guideButton = document.getElementById("guide-button");
-var closeGuideButton = document.getElementById("close-guide-button");
-closeGuideButton.hidden = true;
-
-guideButton.onclick = ev => {
-    document.getElementById("guide-desc").classList.remove("off-screen");
-    closeGuideButton.hidden = false;
-    guideButton.hidden = true;
-}
-closeGuideButton.onclick = ev => {
-    document.getElementById("guide-desc").classList.add("off-screen");
-    closeGuideButton.hidden = true; 
-    guideButton.hidden = false;
-}
+removeClassToOnclick('off-screen', '#guide-desc', '#guide-button');
+addClassToOnclick('off-screen', '#guide-desc', '#close-guide-button');
